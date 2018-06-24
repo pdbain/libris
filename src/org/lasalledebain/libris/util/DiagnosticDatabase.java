07d737d91390786c82b4507e7cbd5dd3994743ab
Peter Bain <pdbain@acm.org>
2018-06-23 21:05:05 -0400

    Fixed test bugs.

--- src/org/lasalledebain/libris/util/DiagnosticDatabase.java
+++ src/org/lasalledebain/libris/util/DiagnosticDatabase.java
@@ -3,11 +3,13 @@ package org.lasalledebain.libris.util;
 import java.io.File;
 import java.util.HashMap;
 
+import org.lasalledebain.libris.DatabaseAttributes;
 import org.lasalledebain.libris.LibrisDatabase;
 import org.lasalledebain.libris.XmlMetadata;
 import org.lasalledebain.libris.exception.DatabaseException;
 import org.lasalledebain.libris.exception.LibrisException;
 import org.lasalledebain.libris.ui.HeadlessUi;
+import org.lasalledebain.libris.xmlUtils.ElementManager;
 
 public class DiagnosticDatabase extends LibrisDatabase {
 
@@ -18,7 +20,15 @@ public class DiagnosticDatabase extends LibrisDatabase {
 		attrs.put(XML_DATABASE_NAME_ATTR, "unknown");
 		attrs.put(XML_DATABASE_SCHEMA_NAME_ATTR, "unknown");
 		attrs.put(XML_SCHEMA_VERSION_ATTR, "unknown");
-		metadata = new XmlMetadata(this);
+		xmlAttributes = new DatabaseAttributes(this, attrs);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.lasalledebain.libris.LibrisDatabase#fromXml(org.lasalledebain.libris.xmlUtils.ElementManager)
+	 */
+	@Override
+	public void fromXml(ElementManager librisMgr) throws LibrisException {
+		super.fromXml(librisMgr);
 		metadata.setLastRecordId(1);
 	}
 }
